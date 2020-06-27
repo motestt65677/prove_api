210da4f713b4a39524c0f60ccd482b2ba22d9efb
David Cheng <pkpk99887766@gmail.com>
2020-06-27 11:03:52 +0800

    added image to popup on button click

---
+++ resources/views/main/index.blade.php
@@ -0,0 +1,9 @@
+<div>
+    <iframe id="thisFrame" src="http://34.80.28.188/" style="width: 80%; height: 100%; position: relative; top:0;">
+    </iframe>
+</div>
+
+<script src="/js/iframe2image.min.js"></script>
+<script>
+iframe2image(myIframe, cb);
+</script>
